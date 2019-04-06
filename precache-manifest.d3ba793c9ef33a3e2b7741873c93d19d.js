0c26774a3e11dee1ba0beeb98737328d9162060a
mpicasso <mbahnaf@gmail.com>
2019-04-06 20:14:55 +0600

    Updates

---
+++ precache-manifest.d3ba793c9ef33a3e2b7741873c93d19d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "d4511884ac91bfc14da1",
+    "url": "/traversycontactmanager/static/js/runtime~main.d4511884.js"
+  },
+  {
+    "revision": "e73c451422e4d508ae1f",
+    "url": "/traversycontactmanager/static/js/main.e73c4514.chunk.js"
+  },
+  {
+    "revision": "1aca9333aa6102620a17",
+    "url": "/traversycontactmanager/static/js/1.1aca9333.chunk.js"
+  },
+  {
+    "revision": "e73c451422e4d508ae1f",
+    "url": "/traversycontactmanager/static/css/main.925471f5.chunk.css"
+  },
+  {
+    "revision": "1aca9333aa6102620a17",
+    "url": "/traversycontactmanager/static/css/1.eb142609.chunk.css"
+  },
+  {
+    "revision": "253a111e7eeaf06c4bf37365513d808d",
+    "url": "/traversycontactmanager/index.html"
+  }
+];
